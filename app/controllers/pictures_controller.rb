4ff8a27e571db38293ac0faa95fc21726c83f791
NXH <neil.hartz@gmail.com>
2013-11-04 14:55:28 -0600

    HW through Part III

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,19 @@
 class PicturesController < ApplicationController
   def show
+    @picture = Picture.find_by({:id => params[:id]})
+  end
+  def index
+  end
+  def new
+  end
+  def create
+    @p = Picture.new
+    @p.caption = params[:caption]
+    @p.source = params[:source]
+    @p.save
+  end
+  def destroy
+    @picture = Picture.find_by({:id => params[:id]})
+    @picture.destroy
   end
 end
